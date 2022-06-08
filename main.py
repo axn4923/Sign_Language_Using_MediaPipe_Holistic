f5eb32fd94efdc3f0d9717f0023bd8c5e62b88ff
axn4923 <nguyenande@gmail.com>
2022-06-08 00:02:06 -0500

    Detection and Raw Data Collection

---
+++ main.py
@@ -0,0 +1,190 @@
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+import pandas as pd
+
+# Key points using MP Holistic
+from mediapipe.python.solutions import holistic
+from numpy import asarray
+
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False  # Image is no longer writeable
+    results = model.process(image)  # Make prediction
+    image.flags.writeable = True  # Image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+
+def draw_landmarks(image, results):  # FACEMESH_TESSELATION for full face, FACEMESH_CONTOURS for outline face
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)  # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)  # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS)  # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks,
+                              mp_holistic.HAND_CONNECTIONS)  # Draw right hand connections
+
+
+def draw_styled_landmarks(image,
+                          results):  # This function changes the color and thickness of lines and circles of landmarks
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+    # Draw right hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+# cap = cv2.VideoCapture(0)  # This grabs video capture device zero which is ideally our webcam
+# # Set MediaPipe model
+# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+#     while cap.isOpened():
+#
+#         # Read feed
+#         ret, frame = cap.read()
+#
+#         # Make detections
+#         image, results = mediapipe_detection(frame, holistic)
+#         print(results)
+#
+#         # Draw landmarks
+#         draw_styled_landmarks(image, results)
+#
+#         # Show to screen
+#         cv2.imshow('OpenCV Feed', image)
+#
+#         # Break gracefully
+#         if cv2.waitKey(10) & 0xFF == ord('q'):
+#             break
+#     cap.release()
+#     cv2.destroyAllWindows()
+
+
+# draw_landmarks(frame, results)
+
+# plt.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+# pose = []
+# for res in results.pose_landmarks.landmark:
+#    test = np.array([res.x, res.y, res.z, res.visibility])
+#    pose.append(test)
+# if and else statement gives us error handling when face, hands, or body pose is not detected. (set every landmark data to 0)
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33 * 4)
+    face = np.array([[res.x, res.y, res.z] for res in
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468 * 3)
+    lh = np.array([[res.x, res.y, res.z] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 3)
+    return np.concatenate([pose, face, lh, rh])  # this returns 1662 different key points of landmarks in a numpy array
+
+
+# Set Up Folders for Data Collection
+# Path for exported data, numpy arrays
+DATA_PATH = os.path.join('MP_Data')
+CSV_PATH = os.path.join('CSV_Data')
+
+# Actions that we try to detect
+actions = np.array(['hello', 'thanks', 'iloveyou']) # 'sleep', 'finish'
+
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+
+# Folder start
+start_folder = 30
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+            os.makedirs(os.path.join(CSV_PATH, action, str(sequence)))
+        except:
+            pass
+
+# Collect Key Point Values for Training and Testing
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # NEW LOOP
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
+
+                # Read feed
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+
+                # NEW Apply wait logic
+                if frame_num == 0:
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(700)
+                else:
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+
+                # NEW Export keypoints
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)  # saved as npy file in MP_DATA folder
+
+                csv_path = os.path.join(CSV_PATH, action, str(sequence), str(frame_num))
+                pd.DataFrame(keypoints).to_csv(csv_path)  # saved as csv file in CSV_DATA folder
+                # np.savetxt(csv_path, keypoints)  # this line save the keypoints in a txt file MP_DataFolder
+                # Each file has 30 frames of data. Each frame contains 1662 key points (xyz (visability for face only))
+                # from landmarks of pose(33*4), face(468*3), left hand(21*3), right hand(21*3), respectively.
+
+                # print(npy_path)
+                # print(keypoints)
+
+                # Break gracefully
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+
+    cap.release()
+    cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
